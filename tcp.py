fd149e8a543c61a153a8a6a85a92622ef0b545fa
why <3298227484@qq.com>
2025-04-13 19:30:00 +0800

    Update tcp.py

--- tcp.py
+++ tcp.py
@@ -74,5 +74,5 @@ if __name__ == "__main__":
   uvicorn.run(app,host="127.0.0.1",port=8000)
 
 
-# the writer country is china  ,so "print"or "#" follow if Chinese， please translation 。
+# the writer country is China  ,so "print"or "#" follow if Chinese， please translation 。
 
